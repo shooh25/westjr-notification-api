5e10fd4f30ece6e4378255edfd99e2bf7ecdb86e
shooh25 <koan6210@outlook.jp>
2025-08-09 17:51:19 +0900

    APIの骨組み完成

--- app/main.py
+++ app/main.py
@@ -1,7 +1,46 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+import httpx
+import os
+from dotenv import load_dotenv
+from app.westjr_client import get_attention_messages
 
+load_dotenv()
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello, Docker + FastAPI!"}
+# メッセージをLINEに返信するための設定
+LINE_API_REPLY_URL = "https://api.line.me/v2/bot/message/reply"
+HEADERS = {
+    "Content-Type": "application/json",
+    "Authorization": f"Bearer {os.getenv('LINE_CHANNEL_ACCESS_TOKEN')}"
+}
+
+# Lineからメッセージを受信する際のエンドポイント
+# reply_token : 即座に返信するために必要なトークン
+@app.post("/")
+async def line_webhook(request: Request):
+    body = await request.json()
+    events = body.get("events", [])
+    print(body)
+    for event in events:
+        if event["type"] == "message":
+            reply_token = event["replyToken"]
+            user_message = event["message"]["text"].strip()
+            words = user_message.split()
+            if len(words) == 2:
+                attention_messages = get_attention_messages(words[0], int(words[1]))
+                response_text = "\n".join(attention_messages)
+            else:
+                response_text = "正しくありません"
+            
+            payload = {
+                "replyToken": reply_token,
+                "messages": [
+                    {
+                        "type": "text",
+                        "text": response_text
+                    }
+                ]
+            }
+
+            async with httpx.AsyncClient() as client:
+                await client.post(LINE_API_REPLY_URL, headers=HEADERS, json=payload)
---
+++ app/westjr_client.py
@@ -0,0 +1,63 @@
+import westjr
+from const import TARGET_DIRECTIONS, TARGET_TYPES, TARGET_LINES
+
+# 遅延が発生している列車情報を取得する
+def get_delay_trains(target_line, target_direction):
+    jr = westjr.WestJR(area="kinki")
+    delay_trains = []
+    try:
+        trains = jr.get_trains(line=target_line).trains
+        info = jr.get_traffic_info()
+        # print(info)
+        for tr in trains:
+            if tr.direction == target_direction and tr.type in TARGET_TYPES:
+                prev, next = jr.convert_pos(train=tr, line=target_line)
+                if tr.delayMinutes >= 0:
+                    delay_trains.append({
+                        "prev": prev,
+                        "next": next,
+                        "type": tr.displayType,
+                        "delay": tr.delayMinutes,
+                        "dest": tr.dest.text
+                    })
+        return {
+            "status": "success",
+            "message": "遅延情報を取得しました",
+            "data": delay_trains
+        }
+    except Exception as e:
+        return {
+            "status": "error",
+            "message": str(e),
+        }
+
+# 遅延した列車情報をもとにメッセージを生成する
+def get_attention_messages(target_line, target_direction):
+    if target_direction not in TARGET_DIRECTIONS or target_line not in TARGET_LINES:
+        return ["無効な路線または方向です。"]
+    request = get_delay_trains(target_line, target_direction)
+    if request["status"] == "error":
+        return [request["message"]]
+    trains = request["data"]
+    if len(trains) >= 3:
+        messages = []
+        for train in trains:
+            prev = train['prev']
+            next_ = train['next']
+            dest = train['dest']
+            train_type = train['type']
+            delay = train['delay']
+            if next_ is None:
+                messages.append(f"{prev}停車中の{dest}行き{train_type}が{delay}分遅れ")
+            else:
+                messages.append(f"{prev} → {next_}間の{dest}行き{train_type}が{delay}分遅れ")
+        return messages
+    return ["遅延情報はありません"]
+
+# 呼び出し例
+if __name__ == "__main__":
+    target_line = "nara"
+    target_direction = 0
+
+    messages = get_attention_messages(target_line, target_direction)
+    print(messages)
